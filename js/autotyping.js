c2382cb19b59d9e35a39c34f2ea532f38ad8e27b
Taraknath Karan <taraknath341@gmail.com>
2026-08-24 19:48:22 +0530

    Add Recent Skill & remove Nav option

--- js/autotyping.js
+++ js/autotyping.js
@@ -3,25 +3,26 @@
 Dev Name: Taraknath Karan.
 Company: Tarak Program.
 Portfolio: https://taraknath341.github.io
+Github: https://github.com/taraknath341
 */
 let h1 = document.querySelector("#autotyping"),
-  inp = h1.innerHTML,
-  a = new String(inp),
-  countD = -1,
-  b,
-  x = "";
+   inp = h1.innerHTML,
+   a = new String(inp),
+   countD = -1,
+   b,
+   x = "";
 function autoTypeing(word, vaR) {
-  countD++;
-  b = word[countD]
-  if (b == undefined) {
-    clearInterval(typingProcess);
-  } else {
-    x += b;
-    vaR.innerHTML = x;
-  }
+   countD++;
+   b = word[countD];
+   if (b == undefined) {
+      clearInterval(typingProcess);
+   } else {
+      x += b;
+      vaR.innerHTML = x;
+   }
 }
 function subFunc() {
-  autoTypeing(a, h1);
-  autoTypeing(a, h1);
+   autoTypeing(a, h1);
+   autoTypeing(a, h1);
 }
 let typingProcess = setInterval(subFunc, 1);
